7a68ba1fe5ebe7499ff05835931d2eb2f1012cfa
Hernan Cosi <kodek@eat1337.com>
2022-11-12 12:53:15 -0800

    Run flatcar-install as root.

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 # Transpile config to ignition
 docker run --rm -i quay.io/coreos/butane:latest < example.yaml > ignition.json
 # Install flatcar as per https://flatcar-linux.org/docs/latest/installing/bare-metal/installing-to-disk/
-flatcar-install -d /dev/sda -i ignition.json
+sudo flatcar-install -d /dev/sda -i ignition.json
